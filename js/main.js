ddfbdcabf2f52b3fc7196096052be145327ccc25
Agnieszka Wojtasik <wojtasikagnieszka2626@gmail.com>
2018-10-15 20:00:30 +0200

    zmiana

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 let num1 = 2;
 let num2 = 5;
 
-function add(num1, num2) {
-    let result = num1 + num2;
-    console.log(result);
+
+
+function porownanie(num1, num2){
+if (num1>num2){
+console.log(num1);
+} else { 
+console.log(num2);
 }
+}
+porownanie(num1, num2);
+
+
